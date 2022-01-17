c7ed351cfed3e5b81b0519e8ee4b8e171f7ccb83
g.butyaev <g.butyaev@rt-solar.ru>
2022-01-18 00:29:00 +0300

    added new files

---
+++ terraform/modules/app/variables.tf
@@ -0,0 +1,11 @@
+variable public_key_path {
+  # Описание переменной
+  description = "~/.ssh/appuser.pub"
+}
+variable subnet_id {
+  description = "e9bpq95tsdhfkf01k49n"
+}
+variable app_disk_image {
+  description = "fd8ne0hvg0ajkuegb8sa"
+  default     = "fd8ne0hvg0ajkuegb8sa"
+}
---
+++ terraform/modules/db/variables.tf
@@ -0,0 +1,11 @@
+variable public_key_path {
+  # Описание переменной
+  description = "~/.ssh/appuser.pub"
+}
+variable subnet_id {
+  description = "e9bpq95tsdhfkf01k49n"
+}
+variable db_disk_image {
+  description = "fd8sg3tr2gq1vpnru4tj"
+  default     = "fd8sg3tr2gq1vpnru4tj"
+}
